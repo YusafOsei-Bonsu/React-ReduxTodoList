05044f0be8d372f81601c9624872a946df6c9683
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-24 11:34:08 +0100

    logo, finished creating and styling header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import "../styles/Header.css";
 
 const Header = () => {
     return (
-        <header><h2>React/Redux To-do List</h2></header>
+        <header>
+            <img src="assets/react-redux.png" alt="logo" height="50px" width="50px"/>
+            <h1>React/Redux To-do List</h1>
+        </header>
     );
 }
 
