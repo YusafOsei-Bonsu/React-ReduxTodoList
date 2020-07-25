1d6dfb16860e3526fbac599284518ec43a030a08
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-25 18:21:06 +0100

    created a container for the to-do items & applied initial styling to it

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/TodoList.css";
+
+const TodoList = () => {
+    return (
+        <div id="todo-container">
+            <p>This is the container that encapsulates the to-do items.</p>
+        </div>
+    );
+}
+
+export default TodoList;
