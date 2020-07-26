eadf18d35c603769c77c70bf5c79c7788e131d46
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-26 11:31:55 +0100

    Can now display to-do data from the redux store

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import "../styles/TodoList.css";
 
-const TodoList = () => {
-    return (
-        <div id="todo-container">
-            <p>This is the container that encapsulates the to-do items.</p>
-        </div>
-    );
+class TodoList extends React.Component {
+    
+    render() {
+        const { todos } = this.props;
+        return (
+            <div id="todo-container">
+            {/* If the to-do list isn't empty, then display the to-do items. 
+                Otherwise, inform the user that the list is empty.  */}
+                {todos.length > 0 ? todos.map(todo => {
+                    return (
+                        <div>
+                            <p>{todo.name}</p>
+                        </div>
+                    );
+                }) : <p>No to-do items at the moment</p>}
+            </div>
+        );
+    }
+
 }
 
-export default TodoList;
+const mapStateToProps = (state) => {
+    return {
+        todos: state.todos
+    }
+}
+
+export default connect(mapStateToProps)(TodoList);
