05235fb1b4100920fb1eeeb3928da418d7858fa6
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-26 13:35:17 +0100

    Removed date from Todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import "../styles/Todo.css";
 
 const Todo = ({ task }) => {
-    let date = Date.now();
-    let currentDate = date.toString();
-
+    
     return (
         <div className="todo-item">
             <p>{ task }</p>
-            <p>Date added: { currentDate }</p>
         </div>
     );
 }
