1dc6f787b83cd8fdfd6a586067bc56625be91799
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-26 12:15:45 +0100

    Showing the time of entry

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import "../styles/Todo.css";
 
 const Todo = ({ task }) => {
+    let date = Date.now();
+    let currentDate = date.toString();
+
     return (
         <div className="todo-item">
             <p>{ task }</p>
+            <p>Date added: { currentDate }</p>
         </div>
     );
 }
