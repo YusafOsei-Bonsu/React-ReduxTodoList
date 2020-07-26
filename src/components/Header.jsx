f7122bce7803cc4ec1d2ecd2f1cfce81c2bb7aca
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-26 20:44:46 +0100

    Modified Header and its styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import "../styles/Header.css";
 const Header = () => {
     return (
         <header>
-            <img src="assets/react-redux.png" alt="logo" height="50px" width="50px"/>
-            <h1>React/Redux To-do List</h1>
+            <img src="assets/react-redux.png" alt="logo" />
+            <h1>To-do List (React/Redux)</h1>
         </header>
     );
 }
