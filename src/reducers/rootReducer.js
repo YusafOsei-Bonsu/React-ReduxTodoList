b664528a6eecd2184605c6bcfcef64c1b41a7394
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-26 21:04:17 +0100

    Action type to delete todos

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -11,6 +11,13 @@ const rootReducer = (state=initialState, action) => {
                 ...state,
                 todos: [...state.todos, action.todo]
             };
+        // Removes a to-do item from the list
+        case "DELETE_TODO":
+            let newList = state.todos.filter(todo => todo.id !== action.id);
+            return {
+                ...state,
+                todos: newList
+            };
         default:
             return state;       
     }
