0550cf8c77571edb3f6994c4117196cf81297038
Yusaf Osei-Bonsu <yusafoseibonsu@hotmail.com>
2020-07-26 21:04:00 +0100

    Connecting To-do component to the store

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import "../styles/Todo.css";
 
-const Todo = ({ task }) => {
+const Todo = ({ key, task }) => {
+
+    const removeTodo = () => {
+        
+    }
     
     return (
         <div className="todo-item">
             <p>{ task }</p>
+            <button className="btn btn-danger" onClick={removeTodo}>Remove</button>
         </div>
     );
 }
 
-export default Todo;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteTodo: (id) => dispatch({ type: "DELETE_TODO", id }),
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Todo);
